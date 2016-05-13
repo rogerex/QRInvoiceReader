a6d700ee85df9f8dfcefc29ab2e8598e3bbd7bb3
Roger Limachi Orellana <rogerex.bo@gmail.com>
2016-05-13 16:46:29 -0400

    Adding Invoices Activity, moving main to Invoices, creating invoices toolbar.

--- app/src/main/java/bo/bumbay/qrinvoicereader/MainActivity.java
+++ app/src/main/java/bo/bumbay/qrinvoicereader/MainActivity.java
@@ -1,46 +1,20 @@
 package bo.bumbay.qrinvoicereader;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
-    private static final int REQUEST_WRITE_STORAGE = 112;
-
-    private ArrayList<String> invoices;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //set the main content layout of the Activity
-        invoices = new ArrayList<>();
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(myToolbar);
     }
 
@@ -55,10 +29,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_qr_code:
-                readQRCode();
+            case R.id.action_main_add_form:
                 return true;
-            case R.id.action_settings:
+            case R.id.action_main_settings:
                 settings();
                 return true;
             default:
@@ -69,112 +42,5 @@ public class MainActivity extends AppCompatActivity {
     public void settings() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
-    }
-
-    public void writeFileActionPerformed(View v) {
-        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
-        if (!hasPermission) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_WRITE_STORAGE);
-        } else {
-            WriteFile();
-        }
-    }
-
-    public void readQRCode() {
-        try {
-            Intent intent = new Intent(ACTION_SCAN);
-            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-            startActivityForResult(intent, 0);
-        } catch (ActivityNotFoundException ex) {
-            showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
-        }
-    }
-
-    //alert dialog for downloadDialog
-    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
-        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
-        downloadDialog.setTitle(title);
-        downloadDialog.setMessage(message);
-        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                try {
-                    act.startActivity(intent);
-                } catch (ActivityNotFoundException ex) {
-
-                }
-            }
-        });
-        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialogInterface, int i) {
-            }
-        });
-        return downloadDialog.show();
-    }
-
-    //on ActivityResult method
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                //get the extras that are returned from the intent
-                String contents = intent.getStringExtra("SCAN_RESULT");
-                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
-                toast.show();
-
-                invoices.add(contents);
-            }
-        }
-    }
-
-    private void writeToFile(String data) {
-        try {
-            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "bills.txt");
-            file.createNewFile();
-
-            if(file.exists()) {
-                FileOutputStream outputFile = new FileOutputStream(file);
-
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputFile);
-                outputStreamWriter.write(data);
-                outputStreamWriter.close();
-            }
-        }
-        catch (IOException e) {
-            Toast toast = Toast.makeText(this, "Cannot save the file.", Toast.LENGTH_LONG);
-            toast.show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode)
-        {
-            case REQUEST_WRITE_STORAGE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    WriteFile();
-                } else
-                {
-                    Toast.makeText(this, "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    private void WriteFile() {
-        String rows = new String();
-        for (String row:
-                invoices) {
-            rows += row + "\n";
-        }
-        invoices = new ArrayList<>();
-
-        writeToFile(rows);
     }
 }
