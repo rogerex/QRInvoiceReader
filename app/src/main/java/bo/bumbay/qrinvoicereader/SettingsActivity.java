d32b39891400f79d68185642cb31c06fe22d0123
Roger Limachi Orellana <rogerex.bo@gmail.com>
2016-05-13 16:02:30 -0400

    Adding Tool bar and Back Button on Preferences UI.

--- app/src/main/java/bo/bumbay/qrinvoicereader/SettingsActivity.java
+++ app/src/main/java/bo/bumbay/qrinvoicereader/SettingsActivity.java
@@ -3,6 +3,10 @@ package bo.bumbay.qrinvoicereader;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
@@ -10,6 +14,17 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.basic_toolbar, root, false);
+        root.addView(bar, 0);
+        bar.setTitle(this.getTitle());
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -19,6 +34,13 @@ public class SettingsActivity extends PreferenceActivity {
 
     @Override
     public boolean isValidFragment(String fragment) {
+        if (fragment.contains("Contributor"))
+            this.setTitle("Contributor");
+        else if (fragment.contains("Default"))
+            this.setTitle("Default Values");
+        else if (fragment.contains("Validation"))
+            this.setTitle("Validations");
+
         return true;
     }
 
